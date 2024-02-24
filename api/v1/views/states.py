6f949c09310b6a2bb0fe06704731c371894cb187
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-25 00:00:10 +0200

    tasks

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -17,40 +17,36 @@ def all_states():
     return jsonify(all_states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'],
-                 strict_slashes=False)
-def state_by_id(state_id):
-    """Retrieves the list of State object by its id"""
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    return jsonify(state.to_dict())
-
-
-# @app_views.route('/states/<state_id>', methods=['DELETE'],
+# @app_views.route('/states/<state_id>', methods=['GET'],
 #                  strict_slashes=False)
-# def delete_state_by_id(state_id):
-#     """delete State object by its id"""
-#     dict = {}
+# def state_by_id(state_id):
+#     """Retrieves the list of State object by its id"""
 #     state = storage.get(State, state_id)
-#     if state is None:
+#     if not state:
 #         abort(404)
-#     storage.delete(state)
-#     storage.save()
-#     return jsonify(dict), 200
-@app_views.route('/states/<state_id>', methods=['DELETE'])
-def delete_state(state_id):
-    '''Deletes a State object'''
-    all_states = storage.all(State).values()
+#     return jsonify(state.to_dict())
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    '''Retrieves a State object'''
+    all_states = storage.all("State").values()
     state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
     if state_obj == []:
         abort(404)
-    state_obj.remove(state_obj[0])
-    for obj in all_states:
-        if obj.id == state_id:
-            storage.delete(obj)
-            storage.save()
-    return jsonify({}), 200
+    return jsonify(state_obj[0])
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state_by_id(state_id):
+    """delete State object by its id"""
+    dict = {}
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify(dict), 200
+
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
