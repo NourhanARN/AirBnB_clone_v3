751bdd8ae3ae19589ae94a4aef77604077301829
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-24 20:14:33 +0200

    tasks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,31 @@
 """create a route /status on the object app_views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"users": User, "places": Place, "states": State,
+           "cities": City, "amenities": Amenity, "reviews": Review}
 
 
 @app_views.route("/status", methods=['GET'])
 def status():
+    """ returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'])
+def count_stats():
+    """retrieves the number of each objects by type"""
+    result = {}
+    for class_name, cls in classes.items():
+        count = storage.count(cls)
+        result[class_name] = count
+    return jsonify(result)
