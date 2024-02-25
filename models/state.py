103085de99f5b96d6fb15b34ca067d2a1edf9285
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-25 17:48:35 +0200

    tasks

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     if models.storage_t == "db":      
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+                              cascade="delete")
     else:
         name = ""
 
