2f5d171424451956c12f6c93c928310b0f6569df
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-26 00:09:02 +0200

    tasks

--- models/user.py
+++ models/user.py
@@ -30,10 +30,10 @@ class User(BaseModel, Base):
         super().__init__(*args, **kwargs)
         if 'password' in kwargs:
             self.password = self.hash_password(kwargs['password'])
-        
+
     def hash_password(password):
         """hash password"""
         hasher = hashlib.md5()
         hasher.update(password.encode('utf-8'))
-        hashed_password = hasher.hexdigest()   
+        hashed_password = hasher.hexdigest()
         return hashed_password
