37acbf0ffe8391f00addbb1dea528a35090a4805
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-26 00:14:35 +0200

    tasks

--- models/base_model.py
+++ models/base_model.py
@@ -61,7 +61,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, fileStorage=False):
+    def to_dict(self, save_to_file=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -71,7 +71,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if fileStorage is False:
+        if save_to_file is False:
             if "password" in new_dict:
                 del new_dict["password"]
         return new_dict
