bb820d8aacce3ba705c312efbd3e8fc08ba7e74d
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-26 01:04:14 +0200

    tasks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,8 @@
 """create blueprint"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 if app_views is not None:
     from api.v1.views.index import *
@@ -11,4 +12,6 @@ if app_views is not None:
     from api.v1.views.amenities import *
     from api.v1.views.users import *
     from api.v1.views.places import *
+    from api.v1.views.places_amenities import *
     from api.v1.views.places_reviews import *
+    
